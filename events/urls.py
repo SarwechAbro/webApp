fec386770fd0482f3c8d97b891ad0d2a6ea37138
SarwechAbro <sarwechabro@gmail.com>
2025-01-09 00:07:27 +0500

    changes

--- events/urls.py
+++ events/urls.py
@@ -2,12 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	# Path Converters
-	# int: numbers
-	# str: strings
-	# path: whole urls /
-	# slug: hyphen-and_underscores_stuff
-	# UUID: universally unique identifier
 
     path('', views.home, name="home"),
 	path('<int:year>/<str:month>/', views.home, name="home"),
--- webApp/settings.py
+++ webApp/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'newsApp.content.navbar_data'
+        
             ],
         },
     },
